a0ed789379e79f7c1f7020ca90a59c24f0268fc8
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-05-12 22:08:36 +0800

    Final request again

--- UserSideWithCalculation/Form1.vb
+++ UserSideWithCalculation/Form1.vb
@@ -1,10 +1,18 @@
-﻿Public Class Form1
-    Dim dbConn As New DatabaseConnection
-    Dim calc As New Calculation
-    Dim dbFunc As New DatabaseFunctions
-    Dim shiftStarted As Boolean = False
-    Dim shiftStartTime As DateTime
-    Dim employeeID As String = ""
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+
+Public Class Form1
+    Private dbConn As New DatabaseConnection
+    Private calc As New Calculation
+    Private dbFunc As New DatabaseFunctions
+    Private shiftStarted As Boolean = False
+    Private shiftStartTime As DateTime
+    Private loggedInShiftStartTime As DateTime
+    Private originalLoggedInShiftStartTime As DateTime
+    Private employeeID As String = ""
+    Dim login As New Login
+
+    Private Shared instances As New Dictionary(Of String, Form1)
 
     Public Sub New(ByVal userID As String)
         InitializeComponent()
@@ -16,20 +24,26 @@
         Me.ControlBox = False
     End Sub
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.StartPosition = FormStartPosition.CenterScreen
-        dbConn.TestDatabaseConnection()
+    Public Sub Logout()
+        login.Show()
+        Me.WindowState = FormWindowState.Minimized
+        login.WindowState = FormWindowState.Normal
 
-        If dbConn.TestDatabaseConnection Then
-            MessageBox.Show("Successfully connected to the database.")
-
-            StartShift()
-        End If
     End Sub
+
+    Public Shared Function GetInstance(ByVal userID As String) As Form1
+        If instances.ContainsKey(userID) Then
+            Return instances(userID)
+        Else
+            Dim newInstance As New Form1(userID)
+            instances.Add(userID, newInstance)
+            Return newInstance
+        End If
+    End Function
 
     Private Sub StartShift()
         shiftStarted = True
-        shiftStartTime = DateTime.Now
+        shiftStartTime = DateTime.Now - (DateTime.Now - loggedInShiftStartTime)
         EndShiftButton.Enabled = True
 
         Timer1.Start()
@@ -62,15 +76,32 @@
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Dim shiftDuration As TimeSpan = DateTime.Now - shiftStartTime
+        Dim shiftDuration As TimeSpan = DateTime.Now - shiftStartTime ' Use adjusted shift start time
         SalaryValue.Text = "Shift Duration: " & shiftDuration.ToString("hh\:mm\:ss")
     End Sub
 
     Private Sub CloseButton_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
-        If shiftStarted Then
-            Me.WindowState = FormWindowState.Minimized
-        Else
+        If Application.OpenForms.OfType(Of Form1)().Count() = 1 Then
             Application.Exit()
+        Else
+            Me.Close()
         End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.StartPosition = FormStartPosition.CenterScreen
+        dbConn.TestDatabaseConnection()
+
+        If dbConn.TestDatabaseConnection Then
+            MessageBox.Show("Successfully connected to the database.")
+
+            loggedInShiftStartTime = DateTime.Now
+            originalLoggedInShiftStartTime = loggedInShiftStartTime
+            StartShift()
+        End If
+    End Sub
+
+    Private Sub LogoutButton_Click(sender As Object, e As EventArgs) Handles LogoutButton.Click
+        Logout()
     End Sub
 End Class
